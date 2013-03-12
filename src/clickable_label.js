f257984cbc5f82c45f9b6193b679070c287e4774
Eslam Mostafa <cseslam@gmail.com>
2013-03-12 14:57:00 +0200

    fix copyrights

--- src/clickable_label.js
+++ src/clickable_label.js
@@ -1,8 +1,7 @@
-/*
- * Copyright (c) 2013 Gnome.
+/*/*
+ * Copyright (c) 2013 Eslam Mostafa.
  *
- * Gnome Music is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the
+ * Gnome Music is free software; you can Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
  *
